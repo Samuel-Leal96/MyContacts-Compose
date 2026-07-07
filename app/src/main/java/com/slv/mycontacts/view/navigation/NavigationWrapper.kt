85cdc035ba041ecdbfb1547a86597ca73e0dc7de
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-07-07 17:00:46 -0600

    Fix navigation basic

--- app/src/main/java/com/slv/mycontacts/view/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/slv/mycontacts/view/navigation/NavigationWrapper.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -23,74 +24,74 @@ import com.slv.mycontacts.view.views.AddContactView
 import com.slv.mycontacts.view.views.HomeView
 import kotlinx.coroutines.launch
 
-@Composable
-fun NavigationWrapper(modifier: Modifier) {
-    val navController = rememberNavController()
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope() // Necesario para abrir/cerrar el drawer de forma asíncrona
-
-    ModalNavigationDrawer(
-        modifier = modifier,
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                // Elemento del menú: Inicio
-                NavigationDrawerItem(
-                    label = { Text("Inicio") },
-                    icon = { iconHome() },
-                    selected = false, // Puedes manejar aquí cuál está seleccionado
-                    onClick = {
-                        scope.launch { drawerState.close() }
-                        navController.navigate(HomeScreen) {
-                            // Limpia el backstack para no acumular pantallas de inicio
-                            popUpTo(navController.graph.startDestinationId) { saveState = true }
-                            launchSingleTop = true
-                            restoreState = true
-                        }
-                    }
-                )
-
-                HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
-
-                // Elemento del menú: Agregar Contacto
-                NavigationDrawerItem(
-                    label = { Text("Agregar Contacto") },
-                    icon = {iconAddContact()},
-                    selected = false,
-                    onClick = {
-                        scope.launch { drawerState.close() }
-                        navController.navigate(AddContactScreen)
-                    }
-                )
-            }
-        }
-    ) {
-        // Contenido de la app que interactúa con el Drawer
-        NavHost(navController = navController, startDestination = HomeScreen) {
-
-            composable<HomeScreen> {
-                // Pantalla de Inicio (Nivel superior: muestra hamburguesa para abrir el drawer)
-                HomeView(
-                    onOpenDrawer = {
-                        scope.launch { drawerState.open() }
-                    },
-                    navigateAddContact = {
-                        navController.navigate(AddContactScreen)
-                    }
-                )
-            }
-
-            composable<AddContactScreen> {
-                // Pantalla Secundaria (Muestra flecha hacia atrás para regresar en la pila)
-                AddContactView(
-                    navigateBack = {
-                        navController.popBackStack()
-                    }
-                )
-            }
-        }
-    }
-}
+//@Composable
+//fun NavigationWrapper(modifier: Modifier) {
+//    val navController = rememberNavController()
+//    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+//    val scope = rememberCoroutineScope() // Necesario para abrir/cerrar el drawer de forma asíncrona
+//
+//    ModalNavigationDrawer(
+//        modifier = modifier,
+//        drawerState = drawerState,
+//        drawerContent = {
+//            ModalDrawerSheet {
+//                // Elemento del menú: Inicio
+//                NavigationDrawerItem(
+//                    label = { Text("Inicio") },
+//                    icon = { iconHome() },
+//                    selected = false, // Puedes manejar aquí cuál está seleccionado
+//                    onClick = {
+//                        scope.launch { drawerState.close() }
+//                        navController.navigate(HomeScreen) {
+//                            // Limpia el backstack para no acumular pantallas de inicio
+//                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+//                            launchSingleTop = true
+//                            restoreState = true
+//                        }
+//                    }
+//                )
+//
+//                HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+//
+//                // Elemento del menú: Agregar Contacto
+//                NavigationDrawerItem(
+//                    label = { Text("Agregar Contacto") },
+//                    icon = {iconAddContact()},
+//                    selected = false,
+//                    onClick = {
+//                        scope.launch { drawerState.close() }
+//                        navController.navigate(AddContactScreen)
+//                    }
+//                )
+//            }
+//        }
+//    ) {
+//        // Contenido de la app que interactúa con el Drawer
+//        NavHost(navController = navController, startDestination = HomeScreen) {
+//
+//            composable<HomeScreen> {
+//                // Pantalla de Inicio (Nivel superior: muestra hamburguesa para abrir el drawer)
+//                HomeView(
+//                    onOpenDrawer = {
+//                        scope.launch { drawerState.open() }
+//                    },
+//                    navigateAddContact = {
+//                        navController.navigate(AddContactScreen)
+//                    }
+//                )
+//            }
+//
+//            composable<AddContactScreen> {
+//                // Pantalla Secundaria (Muestra flecha hacia atrás para regresar en la pila)
+//                AddContactView(
+//                    navigateBack = {
+//                        navController.popBackStack()
+//                    }
+//                )
+//            }
+//        }
+//    }
+//}
 
 @Composable
 fun iconHome() {
@@ -102,18 +103,17 @@ fun iconAddContact(modifier: Modifier = Modifier) {
     Icon(imageVector = Icons.Default.Add, contentDescription = "Add Contact")
 }
 
-//@Composable
-//fun NavigationWrapper(){
-//    val navController: NavHostController = rememberNavController()
-//
-//    NavHost(navController = navController, startDestination = HomeScreen){
-//        composable<HomeScreen>{
-//            HomeView(navigateAddContact = {navController.navigate(AddContactScreen)})
-//        }
-//
-//        composable<AddContactScreen> {
-//            TopBarNavigationExample(navigateBack = {navController.popBackStack()})
-//            AddContactView( navigateBack = {navController.popBackStack()}  )
-//        }
-//    }
-//}
+@Composable
+fun NavigationWrapper(modifier: Modifier){
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = HomeScreen){
+        composable<HomeScreen>{
+            HomeView(navigateAddContact = {navController.navigate(AddContactScreen)})
+        }
+
+        composable<AddContactScreen> {
+            AddContactView( navigateBack = {navController.popBackStack()}  )
+        }
+    }
+}
