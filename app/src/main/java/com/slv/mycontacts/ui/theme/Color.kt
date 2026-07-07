6dee69c46d81e82898fe461f5d29d9a921d13090
Samuel Leal Vega <Samuel_Leal96@outlook.com>
2026-07-07 14:39:21 -0600

    Floating action button and colors theme

--- app/src/main/java/com/slv/mycontacts/ui/theme/Color.kt
+++ app/src/main/java/com/slv/mycontacts/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.slv.mycontacts.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val GrayPrimary = Color(0xFF2C3E50)
+val GraySecondary= Color(0xFF34495E)
+val GrayTertiary = Color(0xFF95A5A6)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/com/slv/mycontacts/view/views/HomeView.kt
+++ app/src/main/java/com/slv/mycontacts/view/views/HomeView.kt
@@ -1,23 +1,26 @@
 package com.slv.mycontacts.view.views
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.slv.mycontacts.view.components.MyTopBarNavigation
 
@@ -36,15 +39,38 @@ fun HomeView(
                 isHomeScreen = true, // Muestra el icono de menú hamburguesa
                 onNavigateClick = onOpenDrawer // Abre el menú
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier
+                    .padding(bottom = 16.dp, end = 8.dp)
+                    .size(60.dp),
+                onClick = { navigateAddContact() },
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Default.Add, contentDescription = null)
+            }
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+        ) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = tf_contact,
                 onValueChange = { tf_contact = it },
-                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-                label = { Text("Buscar contacto") }
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                },
+                label = { Text("Buscar contacto") },
+                colors = OutlinedTextFieldDefaults.colors()
             )
         }
     }
